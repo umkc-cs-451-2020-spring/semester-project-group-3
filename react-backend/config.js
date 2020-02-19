3fc5aaf5fe071361460fb2f93b03010242ecc28b
Landon <landonvolkmann@gmail.com>
2020-02-18 21:47:31 -0600

    Config hiding

--- react-backend/config.js
+++ react-backend/config.js
@@ -1,7 +1,7 @@
 var config = {
-  host: "",
-  user: "",
-  password: "",
-  database: ""
+  host: "landonvolkmann.com",
+  user: "landonvolkmann_admin",
+  password: "Gr0up3!2020",
+  database: "landonvolkmann_commerce_project"
 };
 module.exports = config;
