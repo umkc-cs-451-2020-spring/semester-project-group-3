37b621d3fb2b9963003bc59c01b7c49cc58ccf65
Sam Feye <samfeye@gmail.com>
2020-04-01 16:10:54 -0500

    Basic Styling
    For header class, layout class, and the nav column

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import styled from 'styled-components';
+import icon from './icon.png';
 
 const Header = styled.div `
   grid-area: header;
   min-height: 60px;
   width: 100%;
   margin: 0 auto;
-  background: #00cc00;
+  background: #006649;
   text-align: left;
+  color: white;
+  font-size: 80px;
+  font-weight: bold;
+  vertical-align: center;
+  padding-left: 10px;
 `
+
 // Todo Add Logo here
 // https://www.commercebank.com/about-us/media-relations/commerce-logos
 // Hex codes: dark green (006649), light green (74BD43)
 export default () => (
   <Header>
-      Commerce bank
+      Commerce Bank
   </Header>
 )
