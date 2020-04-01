469e491f92e99367173c5daf78ffd9dab797c7f0
Connor <clarson1229@live.com>
2020-04-01 12:49:01 -0500

    commented file

--- src/components/NotificationTriggers/NotificationTriggers.js
+++ src/components/NotificationTriggers/NotificationTriggers.js
@@ -26,13 +26,60 @@ import MenuItem from '@material-ui/core/MenuItem';
 import InputLabel from '@material-ui/core/InputLabel';
 import FormHelperText from '@material-ui/core/FormHelperText';
 import TextField from '@material-ui/core/TextField';
-
+// I added all the inports youll need for material UI table.
+// Most likely i added to much.
+// Do div css styling like below example. 
 const Wrapper = styled.div`
   height: 100%;
   width: 100%;
   border: 1px solid blue;
 
 `
+// Defualt functions for sorting. Leave these the same.
+function desc(a, b, orderBy) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+function stableSort(query, searchCol, array, cmp) {
+  array = query ? array.filter(x => x[searchCol].includes(query)) : array;
+  const stabilizedThis = array.map((el, index) => [el, index]);
+  stabilizedThis.sort((a, b) => {
+    const order = cmp(a[0], b[0]);
+    if (order !== 0) return order;
+    return a[1] - b[1];
+  });
+  return stabilizedThis.map(el => el[0]);
+}
+function getSorting(order, orderBy) {
+  return order === 'desc' ? (a, b) => desc(a, b, orderBy) : (a, b) => -desc(a, b, orderBy);
+}
+// put header here
+const headCells = [
+
+];
+// take data from database and pass into this function that will convert it into an object.
+function createData() {
+  return {};
+}
+// Table header lives here
+function EnhancedTableHead(props) {
+
+}
+// This is where the tool bar lives. The rows selected and what not.
+const EnhancedTableToolbar = props => {
+
+}
+// Meat of table lives here   The table head will render in this function.
+function EnhancedTable(props) {
+
+}
+
+// This is the Defualt Compoent that will export.
 function NotificationTriggers(){
   const [triggerList, setTriggerList] = React.useState([ ]);
   const [loading, setLoading] = React.useState(true);
