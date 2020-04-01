387bb534ce9f677c348cdf0bceaf4df58ef571c4
Connor <clarson1229@live.com>
2020-04-01 12:38:47 -0500

    Imports added

--- src/components/NotificationTriggers/NotificationTriggers.js
+++ src/components/NotificationTriggers/NotificationTriggers.js
@@ -1,5 +1,31 @@
 import React from 'react';
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import clsx from 'clsx';
+import { lighten, makeStyles, withStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TablePagination from '@material-ui/core/TablePagination';
+import TableRow from '@material-ui/core/TableRow';
+import TableSortLabel from '@material-ui/core/TableSortLabel';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+import Checkbox from '@material-ui/core/Checkbox';
+import IconButton from '@material-ui/core/IconButton';
+import Tooltip from '@material-ui/core/Tooltip';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Switch from '@material-ui/core/Switch';
+import DeleteIcon from '@material-ui/icons/Delete';
+import FilterListIcon from '@material-ui/icons/FilterList';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import TextField from '@material-ui/core/TextField';
 
 const Wrapper = styled.div`
   height: 100%;
@@ -13,7 +39,7 @@ function NotificationTriggers(){
 
   return (
     <Wrapper>
-      Compoent Lives here
+      Table Compoent Lives here
     </Wrapper>
   )
 }
