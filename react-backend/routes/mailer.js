979931ea92706596fbd8ed94d018e06d88308643
Sam Feye <samfeye@gmail.com>
2020-04-12 10:32:16 -0500

    Finish backend for forgot password

--- react-backend/routes/mailer.js
+++ react-backend/routes/mailer.js
@@ -11,7 +11,7 @@ const connection = mysql.createPool({
     password: config.password,
     database: config.database
   });
-  
+
 var transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
@@ -20,17 +20,47 @@ var transporter = nodemailer.createTransport({
   }
 });
 
-var mailOptions = {
-  from: 'Group3Commerce2020@gmail.com',
-  to: 'myfriend@yahoo.com',
-  subject: 'Recover password, Commerce Bank',
-  text: 'That was easy!'
-};
+router.post("/", function(req, res, next) {
+    console.log("EMAIL:" + req.query.email);
+    var email = req.query.email;
 
-transporter.sendMail(mailOptions, function(error, info){
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('Email sent: ' + info.response);
-  }
+    query =
+      "select count(*) from Account where email = " +
+      connection.escape(email) + ";";
+  
+    query_2 =
+      "select AES_DECRYPT(password, '" + config.password + "')(CHAR) from Account where email = " +
+      connection.escape(email) + ";";
+
+    connection.getConnection(function(err, connection) {
+    // Executing the MySQL query (select all data from the 'users' table).
+    connection.query(query, function(error, results, fields) {
+      // If some error occurs, we throw an error.
+      if (error) throw error;
+
+      if(results[0] > 0) {
+        connection.query(query2, function(error, results, fields) {
+            // If some error occurs, we throw an error.
+            if (error) throw error;
+
+            var mailOptions = {
+                from: 'Group3Commerce2020@gmail.com',
+                to: connection.escape(email),
+                subject: 'Recover password, Commerce Bank',
+                text: 'Here is your recovered password: ' + results[0]
+            };
+
+            transporter.sendMail(mailOptions, function(error, info){
+                if (error) {
+                    console.log(error);
+                } else {
+                    console.log('Email sent: ' + info.response);
+                }
+            });
+         });
+        } else {
+            res.send("Account was not found...")
+        }
+        });
+    }); 
 });
