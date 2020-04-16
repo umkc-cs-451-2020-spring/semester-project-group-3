ef92ec02221135e9885c2566e8b6afd8cddb6fb9
Sam Feye <samfeye@gmail.com>
2020-04-16 08:59:50 -0500

    Remove accessory get transaction query

--- react-backend/routes/export.js
+++ react-backend/routes/export.js
@@ -46,52 +46,5 @@ router.get("/:associatedAccount", function(req, res, next) {
   }
 });
 
-router.get("/:associatedAccount/:otherColumns", function(req, res, next) {
-  var account = req.params.associatedAccount;
-  console.log("method:" + req.method);
-  console.log("otherCol: " + req.params.otherColumns);
-  var otherColumns = JSON.parse(req.params.otherColumns);
-
-  var conditions = "";
-
-  if (otherColumns.type != undefined) {
-    console.log("TYPE FOUND");
-    var type = otherColumns.type;
-    conditions += "and type = '" + type + "'\n";
-  } else {
-    console.log("TYPE NOT FOUND");
-  }
-
-  if (!account) {
-    console.log("associatedAccount not defined");
-    res.send("associatedAccount not defined");
-  } else {
-    connection.getConnection(function(err, connection) {
-      // Executing the MySQL query (select all data from the 'users' table).
-      connection.query(
-        "select * from Transaction where associatedAccount = " +
-          account +
-          "\n" +
-          conditions +
-          ";",
-        function(error, results, fields) {
-          // If some error occurs, we throw an error.
-          if (error) throw error;
-
-          // Getting the 'response' from the database and sending it to our route. This is were the data is.
-          const jsonData = JSON.parse(JSON.stringify(results));
-          console.log("jsonData", jsonData);
-
-          fastcsv
-          .write(jsonData, { headers: true })
-          .on("finish", function() {
-              res.send("Exported transactions to transactions_fastcsv.csv successfully!");
-            })
-            .pipe(ws);
-        }
-      );
-    });
-  }
-});
 
 module.exports = router;
