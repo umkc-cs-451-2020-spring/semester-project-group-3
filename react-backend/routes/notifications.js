3698138613f64186fd63b9ebbc4ad6021808d3ac
Landon <landonvolkmann@gmail.com>
2020-04-16 15:36:26 -0500

    Detect transactions below balance for given time

--- react-backend/routes/notifications.js
+++ react-backend/routes/notifications.js
@@ -9,6 +9,7 @@ const connection = mysql.createPool({
   user: config.user,
   password: config.password,
   database: config.database,
+  multipleStatements: true,
 });
 
 class NotificationHandler {
@@ -18,48 +19,107 @@ class NotificationHandler {
       user: config.user,
       password: config.password,
       database: config.database,
+      multipleStatements: true,
     });
     this.accountID = this.conn.escape(accountID);
+    this.dateTimeFormat = "'%Y-%m-%d %T'";
   }
 
-  async getNewNotifications() {
+  getNewNotifications() {
+    let self = this;
     const conn = this.conn;
 
     console.log("getNewNotifications entered...");
     var newNotifications = [];
-    //https://codeburst.io/2-async-functions-in-javascript-async-await-a905d5aae73c
 
-    const lastNotificationCheck = await this.getLastNotificationCheck();
+    const getLastNotificationCheckQuery = `select date_format(lastNotificationCheck, ${self.dateTimeFormat}) as lastNotificationCheck from Account where accountID = ${self.accountID};`;
+    const getNotificationTriggersQuery = `select type, amount, value, description from NotificationTrigger where active = true and associatedAccount = ${self.accountID} and startDate < current_timestamp;`;
+    const multiQuery =
+      getLastNotificationCheckQuery + getNotificationTriggersQuery;
 
-    console.log("this is last...");
-    console.log(lastNotificationCheck);
+    conn.getConnection(function (err, conn) {
+      conn.query(multiQuery, function (error, results, fields) {
+        // If some error occurs, we throw an error.
+        if (error) throw error;
 
-    const getTransactionsQuery = "";
+        console.log("\nRESPONSE:");
+        console.log(results);
+        var lastNotificationCheck = results[0][0]["lastNotificationCheck"];
 
-    const getNotificationTriggersQuery = "";
+        console.log("\nLast Notification Check: " + lastNotificationCheck);
+
+        var notificationTriggers = [];
+        const triggerColumnNames = ["type", "amount", "value", "description"];
+
+        console.log("\nTriggers:");
+        results[1].forEach((trigger, idx, array) => {
+          notificationTriggers.push(trigger);
+          console.log("\n" + idx + ":");
+          triggerColumnNames.forEach((col_name) => {
+            console.log(col_name + ": " + trigger[col_name]);
+          });
+        });
+
+        var getTransactionsQuery =
+          "select type, amount, description, historicBalance, date_format(processingDate, " +
+          self.dateTimeFormat +
+          ") as processingDate from Transaction where associatedAccount = " +
+          self.accountID +
+          " and processingDate > str_to_date('" +
+          lastNotificationCheck +
+          "'," +
+          self.dateTimeFormat +
+          ");";
+
+        console.log("\nThen query: " + getTransactionsQuery);
+        conn.query(getTransactionsQuery, function (error, results, fields) {
+          // If some error occurs, we throw an error.
+          if (error) throw error;
+
+          var newTransactions = [];
+          const transactionColumnNames = [
+            "processingDate",
+            "type",
+            "amount",
+            "description",
+            "historicBalance",
+          ];
+
+          console.log("\nTransactions:");
+          results.forEach((transaction, idx, array) => {
+            newTransactions.push(transaction);
+            console.log("\n" + idx + ":");
+            transactionColumnNames.forEach((col_name) => {
+              console.log(col_name + ": " + transaction[col_name]);
+            });
+          });
+
+          var balanceBelowTransactions = self.getIfBalanceBelow(
+            newTransactions,
+            0.0
+          );
+
+          balanceBelowTransactions.forEach((transaction) => {
+            console.log(transaction["description"]);
+          });
+        });
+      });
+    });
 
     return newNotifications;
   }
 
-  async getLastNotificationCheck() {
-    const conn = this.conn;
-    const getLastNotificationCheckQuery = `select lastNotificationCheck from Account where accountID = ${this.accountID};`;
-
-    conn.getConnection(function (err, conn) {
-      console.log("Connection entered");
-      conn.query(getLastNotificationCheckQuery, function (
-        error,
-        results,
-        fields
-      ) {
-        // If some error occurs, we throw an error.
-        if (error) throw error;
-        var x = results[0];
-        console.log("this is x...");
-        console.log(x);
-        return results[0];
-      });
+  getIfBalanceBelow(transactions, minBalance) {
+    var guiltyTransactions = [];
+    transactions.forEach((transaction, idx, array) => {
+      console.log(
+        "Comparing " + transaction["historicBalance"] + " and " + minBalance
+      );
+      if (transaction["historicBalance"] <= minBalance) {
+        guiltyTransactions.push(transaction);
+      }
     });
+    return guiltyTransactions;
   }
 
   archiveNotifications(notifications) {}
