01b9bc9efe0f246f50866bd1a92a173489094082
Samuel Feye <37005498+sfeye@users.noreply.github.com>
2020-04-17 14:05:17 -0500

    Add transaction page PR (#26)
    * Transaction page added
    
    * Add export button
    
    Co-authored-by: Connor <clarson1229@live.com>

--- src/components/Transaction/Transaction.js
+++ src/components/Transaction/Transaction.js
@@ -1,11 +1,71 @@
 import React from 'react';
 import styled from 'styled-components';
+import fetchTransactions from "../Dashboard/transactionAction.js";
+import { useSelector, useDispatch } from 'react-redux';
+import Table from '../Table';
 
 function Transaction(){
+  const dispatch = useDispatch();
+  const axios = require('axios');
+  const acctID = useSelector((state) => state.loginReducer.accountID );
 
+  const handleClick = (event) => {
+    axios.get('/export/' + acctID)
+    .then(function(response) {
+      window.alert(response.data);
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+  }
+
+  function createData(transId, pDate, balance, chargeType, amount, description) {
+    return {transId, pDate, amount, chargeType, balance, description};
+  }
+  function createRows(transaction){
+    var tempRows= [];
+    if (transaction){
+      for (var i = 0; i< transaction.length; i++){
+        // todo add formating to date data.
+        tempRows.push(createData(
+          transaction[i].transactionID,
+          transaction[i].processingDate,
+          transaction[i].historicBalance,
+          transaction[i].type,
+          transaction[i].amount,
+          transaction[i].description
+        ));
+      }
+    }
+    return tempRows;
+  }
+  async function getTransactions(){
+    await dispatch(fetchTransactions(acctID));
+  }
+  // this is called after the component is rendered.
+  React.useEffect(() => {
+   getTransactions();
+  }, []);
+
+  const error = useSelector((state) => state.transactionsReducer.error );
+  const loading = useSelector((state) => state.transactionsReducer.loading );
+  const transactions = useSelector((state) => state.transactionsReducer.items );
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    return <div>Error! {error}</div>;
+  }
   return (
     <div>
-    //component goes here
+      <Table rows={createRows(transactions)}>
+      </Table>
+      <button type="button"
+              className="submit-btn"
+              onClick={handleClick}> 
+      Export to CSV
+      </button>
     </div>
   );
 }
