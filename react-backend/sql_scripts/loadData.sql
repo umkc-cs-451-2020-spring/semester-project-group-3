25dfb96909e3f2b07241957d91b18b846b837f5f
Landon <landonvolkmann@gmail.com>
2020-04-19 08:38:48 -0500

    Encrypted base test account

--- react-backend/sql_scripts/loadData.sql
+++ react-backend/sql_scripts/loadData.sql
@@ -1,48 +1,15 @@
 use landonvolkmann_commerce_project;
 
-insert into
+ insert into
     Account (accountID, email, password, balance)
 values
     (
         "211111110",
         "test@gmail.com",
-        "P@ssword1",        -- update account set password = AES_ENCRYPT("P@ssword1", "Gr0up3!2020") where accountID = "211111110"
+        aes_encrypt("P@ssword1", "Gr0up3!2020"),
         5000.00
     );
 
-call setTransaction(
-    "211111110",
-    "2019-11-01",
-    "DR",
-    2.00,
-    "Starbucks"
-);
-
-call setTransaction(
-    "211111110",
-    "2019-12-01",
-    "DR",
-    35.00,
-    "Gas"
-);
-
-call setTransaction(
-    "211111110",
-    "2020-01-12",
-    "CR",
-    100.00,
-    "Payday"
-);
-
-insert into
-    Account (accountID, email, password, balance)
-values
-    (
-        "211111110",
-        "test2@gmail.com",
-        AES_ENCRYPT("P@ssword1", "Gr0up3!2020"),        -- update account set password = AES_ENCRYPT("P@ssword1", "Gr0up3!2020") where accountID = "211111110"
-        5000.00
-    );
 
 call setTransaction(
     "211111110",
