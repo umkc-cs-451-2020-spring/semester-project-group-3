7a529eb8d4c94a4e962f7689f4f135d3d96b229e
Sam Feye <samfeye@gmail.com>
2020-04-22 07:19:37 -0500

    Adjust signup
    small change to mailer

--- src/components/SignUp/signupAction.js
+++ src/components/SignUp/signupAction.js
@@ -5,15 +5,12 @@ import { setCurrentUser } from '../../rStore/actions/loginActions';
 export default function signup(accountID, email, password) {
     const axios = require('axios');
     return dispatch => {
-        axios.post('/signup?accountID=' + accountID + '&email=' + email +
-        '&password=' + password)
+        axios.post('/signup?email=' + email + '&password=' + password)
         .then(function(response) {
-          if (response.data === 'Success') {
-            dispatch(setCurrentUser(accountID));
-            dispatch(renderApp());
-          }else {
-            window.alert(response.data);
-          }})
+          window.alert("Account was successfully created! Your account ID is: " + response.data);
+          dispatch(setCurrentUser(response.data));
+          dispatch(renderApp());
+          })
         .catch(function(error) {
           console.log(error);
         })
