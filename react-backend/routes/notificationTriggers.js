b52f09cbeca07b80c79fc2ba22518d39867d3fba
Landon <landonvolkmann@gmail.com>
2020-04-24 06:58:32 -0500

    Multi update notif triggers functionality

--- react-backend/routes/notificationTriggers.js
+++ react-backend/routes/notificationTriggers.js
@@ -8,10 +8,11 @@ const connection = mysql.createPool({
   host: config.host,
   user: config.user,
   password: config.password,
-  database: config.database
+  database: config.database,
+  multipleStatements: true,
 });
 
-router.post("/", function(req, res, next) {
+router.post("/", function (req, res, next) {
   var associatedAccount = req.query.account;
   var type = req.query.type;
   var amount = req.query.amount;
@@ -49,8 +50,8 @@ router.post("/", function(req, res, next) {
     connection.escape(description) +
     ");";
 
-  connection.getConnection(function(err, connection) {
-    connection.query(query, function(error, results, fields) {
+  connection.getConnection(function (err, connection) {
+    connection.query(query, function (error, results, fields) {
       // If some error occurs, we throw an error.
       if (error) throw error;
 
@@ -60,7 +61,7 @@ router.post("/", function(req, res, next) {
   });
 });
 
-router.get("/:associatedAccount", function(req, res, next) {
+router.get("/:associatedAccount", function (req, res, next) {
   var account = req.params.associatedAccount;
   console.log("method:" + req.method);
   console.log("body:" + account);
@@ -68,10 +69,10 @@ router.get("/:associatedAccount", function(req, res, next) {
     console.log("associatedAccount not defined");
     res.send("associatedAccount not defined");
   } else {
-    connection.getConnection(function(err, connection) {
+    connection.getConnection(function (err, connection) {
       connection.query(
         "select * from NotificationTrigger where associatedAccount =" + account,
-        function(error, results, fields) {
+        function (error, results, fields) {
           // If some error occurs, we throw an error.
           if (error) throw error;
 
@@ -83,7 +84,7 @@ router.get("/:associatedAccount", function(req, res, next) {
   }
 });
 
-router.post("/delete/", function(req, res, next) {
+router.post("/delete/", function (req, res, next) {
   var notificationTriggerID = req.query.notificationTriggerID;
 
   query =
@@ -91,8 +92,8 @@ router.post("/delete/", function(req, res, next) {
     notificationTriggerID +
     ";";
 
-  connection.getConnection(function(err, connection) {
-    connection.query(query, function(error, results, fields) {
+  connection.getConnection(function (err, connection) {
+    connection.query(query, function (error, results, fields) {
       // If some error occurs, we throw an error.
       if (error) throw error;
 
@@ -100,6 +101,77 @@ router.post("/delete/", function(req, res, next) {
       res.send(true);
     });
   });
+});
+
+router.post("/update/", function (req, res, next) {
+  var triggersToUpdate = req.query.triggers;
+
+  console.log(triggersToUpdate);
+
+  var multiUpdateQuery = "";
+  triggersToUpdate.forEach((trigger, idx, array) => {
+    var notificationTriggerID = trigger["notificationTriggerID"];
+
+    if (notificationTriggerID == undefined) {
+      return true; // acts as a continue statement
+    }
+
+    var type = trigger["type"];
+    var active = trigger["active"];
+    var amount = trigger["amount"];
+    var value = trigger["value"];
+    var startDate = trigger["startDate"];
+
+    var query = "update NotificationTrigger set ";
+
+    if (type != undefined) {
+      type = connection.escape(type);
+      query += `type = ${type}, `;
+    }
+
+    if (active != undefined) {
+      query += `active = ${active}, `;
+    }
+
+    if (startDate != undefined) {
+      startDate = connection.escape(startDate);
+      query += `startDate = ${startDate}, `;
+    }
+
+    // convert undefined to null
+    if (amount == undefined) {
+      amount = null;
+    } else {
+      amount = connection.escape(amount);
+    }
+    query += `amount = ${amount}, `;
+
+    if (value == undefined) {
+      value = null;
+    } else {
+      value = connection.escape(value);
+    }
+    query += `value = ${value} `;
+
+    query += `where notificationTriggerID = ${notificationTriggerID};`;
+
+    multiUpdateQuery += query;
+  });
+
+  if (multiUpdateQuery != "") {
+    connection.getConnection(function (err, connection) {
+      connection.query(multiUpdateQuery, function (error, results, fields) {
+        // If some error occurs, we throw an error.
+        if (error) throw error;
+
+        console.log("Successfully applied update to triggers!");
+        res.send(true);
+      });
+    });
+  } else {
+    console.log("No triggers to update!");
+    res.send(false);
+  }
 });
 
 module.exports = router;
