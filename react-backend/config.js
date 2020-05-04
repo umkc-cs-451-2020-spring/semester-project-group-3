61df9e06d0d601bc8c9a0281f59b0a71377280cc
Samuel Feye <samfeye@gmail.com>
2020-05-04 11:53:51 -0500

    Remove config file so we can DB connection

--- react-backend/config.js
+++
@@ -1,8 +0,0 @@
-var config = {
-  host: "landonvolkmann.com",
-  user: "landonvolkmann_admin",
-  password: "Gr0up3!2020",
-  database: "landonvolkmann_commerce_project",
-  email: "Group3Commerce2020@gmail.com"
-};
-module.exports = config;
